a5f8a800b3128fac0b7dfe2e20a617d37838b228
YJzhangyh <zhangyh@yunjiacloud.com>
2021-11-09 18:23:23 +0800

    feat:添加useMount 和 useDebounce两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { SearchPanel } from "./search-panel"
 import { List } from "./list"
 import { useState, useEffect } from "react"
-import { cleanObject } from "utils"
+import { cleanObject, useDebounce } from "utils"
 import * as  qs from "qs"
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -12,17 +12,18 @@ export const ProjectListScreen = () => {
         name: '',
         personId: ''
     })
+    const debounceParam = useDebounce(param, 2000)
     const [users, setUsers] = useState([])
     const [list, setList] = useState([])
 
 
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response => {
             if (response.ok) {
                 setList(await response.json())
             }
         })
-    }, [param])
+    }, [debounceParam])
     useEffect(() => {
         fetch(`${apiUrl}/users`).then(async response => {
             if (response.ok) {
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 // 在一个函数里 改变传入对象的本身是不好的
@@ -12,4 +14,19 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {}, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    // 每次value变化以后 设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再执行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
 };
