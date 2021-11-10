2f052e2e8df80aa844e7686b19be928a5ed1e13a
YJzhangyh <zhangyh@yunjiacloud.com>
2021-11-10 15:26:11 +0800

    chore:any -> unknown

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value:any) => (value === 0 ? false : !value);
+export const isFalsy = (value:unknown) => (value === 0 ? false : !value);
 
 // 在一个函数里 改变传入对象的本身是不好的
 export const cleanObject = (object:object) => {
@@ -22,7 +22,7 @@ export const useMount = (callback:()=>void) => {
   useEffect(() => {}, []);
 };
 
-export const useDebounce = (value:any, delay?:number) => {
+export const useDebounce = (value:unknown, delay?:number):any => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     // 每次value变化以后 设置一个定时器
