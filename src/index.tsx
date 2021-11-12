31d7a04d24456f3a749f7a3bba74f7f99f51d2a3
YJzhangyh <zhangyh@yunjiacloud.com>
2021-11-12 16:08:28 +0800

    feat:安装imooc-jira-tool 用service worker代理服务端请求

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AppProviders } from 'context';
+import { loadDevTools } from "jira-dev-tool";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <AppProviders>
-      <App />
-    </AppProviders>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+loadDevTools(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <AppProviders>
+        <App />
+      </AppProviders>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
