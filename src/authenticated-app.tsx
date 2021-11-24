02cd5c1db5896935164f383b2178ee9884ccd667
YJzhangyh <zhangyh@yunjiacloud.com>
2021-11-24 15:22:15 +0800

    style:用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,5 +1,5 @@
-
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
 import { useAuth } from "context/auth-context";
 import React from "react";
 import { ProjectListScreen } from "screens/project-list";
@@ -15,55 +15,36 @@ import { ProjectListScreen } from "screens/project-list";
 
 export const AuthenticatedApp = () => {
     const { logout } = useAuth()
-    return <Container>
-        <Header>
-            <HeaderLeft>
-                <h3>logo</h3>
-                <h3>项目</h3>
-                <h3>哈哈哈</h3>
-            </HeaderLeft>
-            <HeaderRight>
-                <button onClick={logout}>登出</button>
-            </HeaderRight>
-
-        </Header>
-        <Nav>nav</Nav>
-        <Main>  <ProjectListScreen /></Main>
-        <Aside>aside</Aside>
-        <Footer>footer</Footer>
-    </Container>
+    return (
+        <Container>
+            <Header between={true}>
+                <HeaderLeft gap={true}>
+                    <h2>logo</h2>
+                    <h2>项目</h2>
+                    <h2>用户</h2>
+                </HeaderLeft>
+                <HeaderRight>
+                    <button onClick={logout}>登出</button>
+                </HeaderRight>
+            </Header>
+            <Main>
+                <ProjectListScreen />
+            </Main>
+        </Container>
+    )
 }
 const Container = styled.div`
 display:grid;
 grid-template-rows:6rem 1fr 6rem;
-grid-template-columns:20rem 1fr 20rem;
-grid-template-areas:
-"header header header"
-"nav main aside"
-"footer footer footer"
-;
 height:100vh;
-grid-gap:10rem;
 `
 
-const Header = styled.header`
-grid-area:header;
-display:flex;
-flex-direction:row;
-align-items:center;
-justify-content:space-between;
+const Header = styled(Row)`
 `
-const HeaderLeft = styled.div`
-display:flex;
-align-items:center;
+const HeaderLeft = styled(Row)`
 `
-const HeaderRight = styled.div`
-
-`
-const Main = styled.main`grid-area:main`
-const Nav = styled.nav`grid-area:nav`
-const Aside = styled.aside`grid-area:aside`
-const Footer = styled.footer`grid-area:footer`
+const HeaderRight = styled.div``
+const Main = styled.main``
 
 
 
